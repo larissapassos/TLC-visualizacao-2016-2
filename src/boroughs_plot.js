d8368701cd0afa2d514bd7eb3256fb0eb1c1fd45
Guilherme Peixoto <gpp@cin.ufpe.br>
2016-11-03 09:30:11 -0300

    feat(boroughs_plot.js): adds boroughs map to index.html

--- src/boroughs_plot.js
+++ src/boroughs_plot.js
@@ -3,17 +3,7 @@ var PICK_LAT = "Pickup_latitude",
     DROP_LAT = "Dropoff_latitude",
     DROP_LNG = "Dropoff_longitude";
 
-function toGeoJSON(datum, key) {
-    var lat = datum[key == "pickup" ? PICK_LAT : DROP_LAT],
-        lng = datum[key == "pickup" ? PICK_LNG : DROP_LNG];
-
-    return {
-        "type": "Feature",
-        "geometry": {"type": "Point", "coordinates": [lng, lat, 0.0]}, //TODO: change coords
-        "properties": {"type": key} //TODO: fix properties
-    };
-}
-
+var svg;
 var width = 960,
     height = 500,
     featureColl = {},
@@ -28,60 +18,75 @@ var path = d3.geoPath()
     .projection(projection)
     .pointRadius(.7);
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+function toGeoJSON(datum, key) {
+    var lat = datum[key == "pickup" ? PICK_LAT : DROP_LAT],
+        lng = datum[key == "pickup" ? PICK_LNG : DROP_LNG];
 
-d3.json("../data/boroughs.geojson", function(error, nycGeoJson) {
-    var group = svg.append("g")
-        .attr("class", "g-1");
+    return {
+        "type": "Feature",
+        "geometry": {"type": "Point", "coordinates": [lng, lat, 0.0]},
+        "properties": {"type": key}
+    };
+}
 
-    group.append("g")
-        .attr("class", "boroughs")
-        .selectAll(".state")
-        .data(nycGeoJson.features)
-        .enter()
-        .append("path")
-        .attr("class", function(d){ return d.properties.name; })
-        .attr("d", path)
-        .style("stroke", "black")
-        .style("stroke-width", ".5px")
-        .style("fill-opacity", ".1");
+function initSVG(){
+    svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("class", "boroughs");
+}
 
-    console.log(nycGeoJson);
-});
+function loadMap(){
+    d3.json("../data/boroughs.geojson", function(error, nycGeoJson) {
+        var group = svg.append("g")
+            .attr("class", "g-1");
 
-//============================================
-//=========== LOAD TLC DATA ==================
-//============================================
-d3.csv("../assets/tlc/green/subset.csv", function(error, tlc){
-    if(!loaded) {
-        var features = [];
-
-        tlc.slice(1, tlc.length).forEach(function(datum){
-            features.push(toGeoJSON(datum, "pickup"));
-            features.push(toGeoJSON(datum, "dropoff"));
-        })
-
-        d3.select(".g-1")
-            .append("g")
-            .attr("class", "tlc")
-            .selectAll("path.point")
-            .data(features)
+        group.append("g")
+            .attr("class", "boroughs")
+            .selectAll(".state")
+            .data(nycGeoJson.features)
             .enter()
             .append("path")
+            .attr("class", function(d){ return d.properties.name; })
             .attr("d", path)
-            .attr("class", "point")
-            .style("fill", d => d.properties.type == "pickup" ? "blue" : "green")
-            .style("fill-opacity", ".2");
+            .style("stroke", "black")
+            .style("stroke-width", ".5px")
+            .style("fill-opacity", ".1");
 
-        var legends = [["pickup", "blue"], ["dropoff", "green"]];
-        var legend = legendSVG
-            .append("g")
-            .attr("class", "legend")
-            .attr("x", 435)
-            .attr("y", 25)
-            .attr("height", 100)
-            .attr("width", 100);
-    }
-});
+        console.log(nycGeoJson);
+    });
+}
+
+
+function loadTaxiSpots(){
+    d3.csv("../assets/tlc/green/subset.csv", function(error, tlc){
+        if(!loaded) {
+            var features = [];
+
+            tlc.slice(1, tlc.length).forEach(function(datum){
+                features.push(toGeoJSON(datum, "pickup"));
+                features.push(toGeoJSON(datum, "dropoff"));
+            })
+
+            d3.select(".g-1")
+                .append("g")
+                .attr("class", "tlc")
+                .selectAll("path.point")
+                .data(features)
+                .enter()
+                .append("path")
+                .attr("d", path)
+                .attr("class", "point")
+                .style("fill", d => d.properties.type == "pickup" ? "blue" : "green")
+                .style("fill-opacity", ".2");
+        }
+    });
+}
+
+function init(){
+    initSVG();
+    loadMap();
+    loadTaxiSpots();
+}
+
+init();
